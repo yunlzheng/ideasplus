c88b62eaf3270a9ad183bf4d63c4a6ac69a3de10
yunlzheng <yunl.zheng@gmail.com>
2014-09-10 17:03:58 +0800

    create basic express example

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+
+
 app.get('/hello.txt', function (req, res) {
     res.send('Hello World');
 });
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+})
 
 var server = app.listen(3000, function () {
     console.log('Listening on port %d', server.address().port);
